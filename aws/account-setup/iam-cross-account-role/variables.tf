c9af5cde9011fbdacc1019610e02c305bb3f9000
Elliott Pope <epope@bu.edu>
2025-11-14 07:53:28 -0500

    Making variable names clearer Bumping the AWS provider version Adding paths to the AWS resources Simplifying names

--- aws/account-setup/iam-cross-account-role/variables.tf
+++ aws/account-setup/iam-cross-account-role/variables.tf
@@ -1,3 +1,9 @@
+variable "role_path" {
+  description = "Path of the IAM role to create"
+  type        = string
+  default     = "/"
+}
+
 variable "role_name" {
   description = "Name of the IAM role to create"
   type        = string
@@ -9,28 +15,33 @@ variable "role_description" {
   default     = "Cross-account IAM role with read-only access to IAM groups and users"
 }
 
+variable "policy_path" {
+  description = "Path of the IAM policy to create"
+  type        = string
+  default     = "/"
+}
+
 variable "policy_name" {
   description = "Name of the IAM policy to create"
   type        = string
 }
 
-variable "trusted_role_arn" {
+variable "skylift_account_reader_iam_role_arn" {
   description = "ARN of the IAM role in the trusted account that can assume this role"
   type        = string
 
   validation {
-    condition     = can(regex("^arn:aws:iam::[0-9]{12}:role/.+", var.trusted_role_arn))
+    condition     = can(regex("^arn:aws:iam::[0-9]{12}:role/.+", var.skylift_account_reader_iam_role_arn))
     error_message = "The trusted_role_arn must be a valid IAM role ARN (e.g., arn:aws:iam::123456789012:role/RoleName)"
   }
 }
 
-variable "external_id" {
+variable "skylift_account_external_id" {
   description = "External ID for assuming the role (used to prevent confused deputy problem)"
   type        = string
-  sensitive   = true
 
   validation {
-    condition     = length(var.external_id) >= 2 && length(var.external_id) <= 1224
+    condition     = length(var.skylift_account_external_id) >= 2 && length(var.skylift_account_external_id) <= 1224
     error_message = "The external_id must be between 2 and 1224 characters"
   }
 }
