2ade0a60948b7804a2de74336a96ea4810619211
Elliott Pope <epope@bu.edu>
2025-11-14 10:02:55 -0500

    Updating docs

--- aws/account-setup/iam-cross-account-role/main.tf
+++ aws/account-setup/iam-cross-account-role/main.tf
@@ -1,3 +1,44 @@
+/**
+* # `skylift.dev` AWS Account Setup Module
+* 
+* This Terraform module creates an AWS IAM role that can be assumed by the `skylift.dev` AWS Account Reader role which synchronizes AWS IAM groups and IAM user memberships
+* 
+* ## Features
+* 
+* - Creates an IAM role that can be assumed by the `skylift.dev` service account role.
+* - `skylift.dev` will pass the unique Account ID as the ExternalID when assuming the role for added security
+* - Grants read-only permissions for IAM operations:
+*   - List and get IAM groups
+*   - Get IAM users
+*   - List groups for users
+*   - Add a user to a group
+*   - Remove a user from a group
+* 
+* ## Usage
+* 
+* ```hcl
+* module "skylift_account_role" {
+*   source = "./aws/account-setup/iam-cross-account-role"
+* 
+*   role_name        = "SkyliftServiceAccount"
+*   policy_name      = "SkyliftPermissions"
+*   # The actual `skylift.dev` AWS IAM Role ARN is available in the docs
+*   trusted_role_arn = "arn:aws:iam::123456789012:role/TrustedRole"
+*   external_id      = "<your Skylift AWS Account ID>"
+* 
+*   tags = {
+*     Environment = "production"
+*     ManagedBy   = "terraform"
+*   }
+* }
+* ```
+* 
+* ## License
+* 
+* This module is provided as-is for use with skylift.dev infrastructure.
+* 
+*/
+
 resource "aws_iam_role" "this" {
   name               = var.role_name
   path               = var.role_path
@@ -46,7 +87,9 @@ data "aws_iam_policy_document" "iam_readonly" {
       "iam:GetGroup",
       "iam:ListGroups",
       "iam:GetUser",
-      "iam:ListGroupsForUser"
+      "iam:ListGroupsForUser",
+      "iam:RemoveUserFromGroup",
+      "iam:AddUserToGroup",
     ]
 
     resources = ["*"]
